13a2de12f3c5ae2b4a2806882a16bb98df85ad4d
kimakowkin <dimatosha@gmail.com>
2023-10-01 22:24:19 +0600

    neo test

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Wake up, Neo...");
+Console.ReadKey();
+Console.Clear();
+
+var str = "The Matrix has you...";
+foreach(char c in str)
+{
+    Console.Write(c);
+    Thread.Sleep(100);
+}
+Console.ReadKey();
+Console.Clear();
+
+str = "Follow the white rabbit.";
+foreach(char c in str)
+{
+    Console.Write(c);
+    Thread.Sleep(50);
+}
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("Knock, knock, Neo.");
+Console.ReadKey();
+Console.Clear();
