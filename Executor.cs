79b9a450c95f7116ce02d94538b43d5bb9265607
kimakowkin <dimatosha@gmail.com>
2023-10-01 22:40:22 +0600

    Added Assembly

---
+++ Executor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+internal class Executor
+{
+    private static void Main(string[] args)
+    {
+        var a = Assembly.LoadFrom(@"C:\dotnet\ConsoleApp\obj\Debug\net7.0\ref\ConsoleApp.dll");
+    }
+}
+
+namespace ConsoleApp
+{
+    internal class Executor
+    {
+    }
+}
